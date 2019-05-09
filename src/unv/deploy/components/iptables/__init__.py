dc1c9c688b3dfff236bec73f364c9b9cf636313a
Morty Space <morty.space@gmail.com>
2019-05-09 23:13:12 +0300

    Updated settings

--- src/unv/deploy/components/iptables/__init__.py
+++ src/unv/deploy/components/iptables/__init__.py
@@ -1,6 +1,22 @@
-class IPtablesComponentSettings:
-    pass
+from unv.deploy.helpers import ComponentSettingsBase
+from unv.deploy.tasks import DeployTasksBase
+from unv.deploy.components.systemd import SystemdTasksMixin
 
 
-class IPtablesTasks:
-    pass
+class IPtablesComponentSettings(ComponentSettingsBase):
+    NAME = 'iptables'
+    DEFAULT = {
+        'bin': '/sbin/iptables-restore',
+        'user': 'root',
+        'rules': 'ipv4.rules',
+        'systemd': {
+            'template': 'app.service',
+            'name': 'iptables_.service',
+            'boot': True,
+            'instances': {'count': 1}
+        }
+    }
+
+
+class IPtablesDeployTasks(DeployTasksBase, SystemdTasksMixin):
+    SETTINGS = IPtablesComponentSettings()
