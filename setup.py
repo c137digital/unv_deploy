8cd6d0c0008ef8d72eb8f47fb7e57bf8863fad74
Morty Space <morty.space@gmail.com>
2019-05-20 22:48:10 +0300

    Fixed deploy to PyPI

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='unv.deploy',
-    version='0.2.36',
+    version='0.2.37',
     description="""Deploy helpers for UNV framework""",
     url='http://github.com/c137digital/unv_deploy',
     author='Morty Space',
